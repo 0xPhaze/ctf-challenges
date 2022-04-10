b2c7ea571a06ee15e75c2e7e5c2af88c593b34f9
0xphaze <0xphaze@gmail.com>
2022-04-10 14:29:07 +0200

    prettier

--- test/exclusive.js
+++ test/exclusive.js
@@ -1,25 +1,30 @@
-const { expect } = require('chai');
-const { ethers, network } = require('hardhat');
+const { expect } = require("chai");
+const { ethers, network } = require("hardhat");
 
-describe('ExclusiveExchange', function () {
+describe("ExclusiveExchange", function () {
+  let deployer, attacker;
   let exchange, nft1, nft2, nft3;
 
-  before('Deploy!', async function () {
-    const ExclusiveExchange = await ethers.getContractFactory('ExclusiveExchange');
-    exchange = await ExclusiveExchange.deploy({ value: ethers.utils.parseEther('50') });
+  before("Deploy!", async function () {
+    [deployer, attacker] = await ethers.getSigners();
+    await ethers.provider.send("hardhat_setBalance", [attacker.address, ethers.utils.parseEther("2")]);
 
-    const ExclusiveNFT = await ethers.getContractFactory('ExclusiveNFT');
+    const ExclusiveExchange = await ethers.getContractFactory("ExclusiveExchange");
+    exchange = await ExclusiveExchange.deploy({ value: ethers.utils.parseEther("50") });
+
+    const ExclusiveNFT = await ethers.getContractFactory("ExclusiveNFT");
     nft1 = ExclusiveNFT.attach(await exchange.memberCollections(1));
     nft2 = ExclusiveNFT.attach(await exchange.memberCollections(2));
     nft3 = ExclusiveNFT.attach(await exchange.memberCollections(3));
   });
 
-  it('Attack!', async function () {
+  it("Attack!", async function () {
     // your attack here
   });
 
-  after('Check!', async function () {
-    console.log('exchange balance', ethers.utils.formatEther(await ethers.provider.getBalance(exchange.address)));
-    expect(await ethers.provider.getBalance(exchange.address)).to.equal(ethers.utils.parseEther('0'));
+  after("Check!", async function () {
+    console.log("exchange balance", ethers.utils.formatEther(await ethers.provider.getBalance(exchange.address)));
+    expect(await ethers.provider.getBalance(exchange.address)).to.equal(ethers.utils.parseEther("0"));
+    expect(await ethers.provider.getBalance(attacker.address)).to.be.gt(ethers.utils.parseEther("50"));
   });
 });
--- test/orderbook.js
+++ test/orderbook.js
@@ -1,20 +1,25 @@
-const { expect } = require('chai');
-const { ethers, network } = require('hardhat');
+const { expect } = require("chai");
+const { ethers, network } = require("hardhat");
 
-describe('ExclusiveExchange', function () {
+describe("OrderBook", function () {
+  let deployer, attacker;
   let orderbook;
 
-  before('Deploy!', async function () {
-    const NFTOrderBook = await ethers.getContractFactory('NFTOrderBook');
-    orderbook = await NFTOrderBook.deploy({ value: ethers.utils.parseEther('50') });
+  before("Deploy!", async function () {
+    [deployer, attacker] = await ethers.getSigners();
+    await ethers.provider.send("hardhat_setBalance", [attacker.address, ethers.utils.parseEther("2")]);
+
+    const NFTOrderBook = await ethers.getContractFactory("NFTOrderBook");
+    orderbook = await NFTOrderBook.deploy({ value: ethers.utils.parseEther("50") });
   });
 
-  it('Attack!', async function () {
+  it("Attack!", async function () {
     // your attack here
   });
 
-  after('Check!', async function () {
-    console.log('orderbook balance', ethers.utils.formatEther(await ethers.provider.getBalance(orderbook.address)));
-    expect(await ethers.provider.getBalance(orderbook.address)).to.be.lt(ethers.utils.parseEther('1'));
+  after("Check!", async function () {
+    console.log("orderbook balance", ethers.utils.formatEther(await ethers.provider.getBalance(orderbook.address)));
+    expect(await ethers.provider.getBalance(orderbook.address)).to.be.lt(ethers.utils.parseEther("1"));
+    expect(await ethers.provider.getBalance(attacker.address)).to.be.gt(ethers.utils.parseEther("50"));
   });
 });
